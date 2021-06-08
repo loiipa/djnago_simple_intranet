7f232f9e4a1e545c0a8562dee9bd50230972465b
hongsam14 <69339846+hongsam14@users.noreply.github.com>
2021-06-08 19:29:09 +0900

    half

--- script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-python3 -m venv env_r01
-source env_r01/bin/activate
-pip3 install -r requirements.txt
--- start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-source env_r01/bin/activate
-python3 manage.py runserver
